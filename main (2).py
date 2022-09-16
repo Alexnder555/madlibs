f7d5f91ad22ce448bef1e8eb7ac5a13341a40c58
Alexnder555 <113711532+Alexnder555@users.noreply.github.com>
2022-09-16 13:10:48 -0400

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,23 @@
+#Write code below that prints a list of word types,
+#asks the user to give an example of each word type,
+#and then stores each user input in its own unique variable:
+print("Today you will be doing a digital madlib")
+adjective1 = input("Tell me an adjective:")
+adjective2 = input("Tell me another adjective:")
+bird = input("Tell me a type of bird:")
+room = input("Tell me a room in your house:")
+verbpast = input("Tell me a past tense verb:")
+verb = input("Tell me a verb in the present tense:")
+relative = input("Tell me the name of a relative:")
+noun = input("Tell me a noun:")
+liquid = input("Tell me a liquid:")
+verbing = input("Tell me a verb that ends in ING:")
+verbing2 = input("Tell me another verb that ends in ING:")
+bodypart = input("Tell me a plural body part:")
+nounplural = input("Tell me a plural noun:")
+noun2 = input("Give one last noun to complete your madlib!:")
+
+
+#print your story in the space below.
+#use your variables from above to fill in the blanks in your story:
+print("It was a " + adjective1 + ", cold November day. I woke up to the " + adjective2 + " smell of " + bird + " roasting in the " + room + " downstairs. I " + verbpast + " down the stairs to see if I could help " + verb + " the dinner. My mom said 'See if " + relative + " needs a fresh " + noun +  "' So I carried a tray of glasses full of " + liquid + " into the " + verbing + " room. When I got there, I couldn't believe my " + bodypart + "! There were " + nounplural + " " + verbing2 + " on the " + noun2 + "!")
